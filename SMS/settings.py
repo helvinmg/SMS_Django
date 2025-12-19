b5507de1ce0b2f44561d559cf7b364bbc8f1e5d5
helvinmg <helvin.m.g@gmail.com>
2025-12-19 11:18:43 +0530

    added admission app

--- SMS/settings.py
+++ SMS/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'exam'
+    'exam',
+    'admission'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS= [BASE_DIR/'static']
-
+print("STATICFILES_DIRS:", STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
