eb232aec43d572a1f9b2a2f578201bfa7e8a96d1
helvinmg <helvin.m.g@gmail.com>
2025-12-19 12:34:14 +0530

    added model

--- admission/models.py
+++ admission/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    slno=models.IntegerField(unique=True)
+    name=models.CharField(max_length=100)
+    course=models.CharField(max_length=100)
+    phonenum=models.CharField(max_length=15)
+    feepaid=models.BooleanField(default=False)
--- admission/views.py
+++ admission/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 def admission(request):
-    return render(request, 'admission_details.html')
+    #fetch data from db
+    records=Student.objects.all()#select * from student
+    return render(request, 'admission_details.html',{'students':records})
